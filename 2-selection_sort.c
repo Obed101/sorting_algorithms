84dc00d692199412a566fc6a8a32d69934df43a3
Anne Machinda <annemachinda@gmail.com>
2021-10-25 15:47:11 +0100

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
 	size_t i,j;
 	for(i = 0; i < size-1; i++)
 	{
-		int min = i;
+		size_t min = i;
 		for(j = i+1; j < size; j++)
 		{
 			if(array[j] < array[min])
