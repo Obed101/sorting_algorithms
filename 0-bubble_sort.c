1a13598556ff7607ace69372c317604cf23f6575
Obed101 <obedamoako92@gmail.com>
2021-10-25 06:00:13 -0700

    compilation fixes

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,15 +12,28 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for(i = 0; i < size; i++)
-	{
-		for(j = 0; j < size - i - 1; j++)
+	if (array[1])
+		for(i = 0; i < size; i++)
 		{
-			if(array[j] > array[j + 1])
+			for(j = 0; j < size - i - 1; j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if(array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
 		}
-	}
+}
+
+/**
+ * swap - function that swaps two integers
+ * @x: first to swap
+ * @y: swapping with @y
+ */
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
