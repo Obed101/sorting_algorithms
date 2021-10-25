c3bbd9a8bc62376b3662871805796342f3e6bab0
Obed101 <obedamoako92@gmail.com>
2021-10-25 07:32:02 -0700

    on it

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,11 +13,11 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 
 	if (array[1])
-		for(i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for(j = 0; j < size - i - 1; j++)
+			for (j = 0; j < size - i - 1; j++)
 			{
-				if(array[j] > array[j + 1])
+				if (array[j] > array[j + 1])
 				{
 					swap(&array[j], &array[j + 1]);
 					print_array(array, size);
@@ -33,7 +33,7 @@ void bubble_sort(int *array, size_t size)
  */
 void swap(int *x, int *y)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+	int temp = *x;
+	*x = *y;
+	*y = temp;
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,29 +11,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-
-size_t i,j;
-for(i = 0; i < size-1; i++)
-  
-{
-
-int min = i;
-        for(j = i+1; j < size; j++)
+	size_t i,j;
+	for(i = 0; i < size-1; i++)
+	{
+		int min = i;
+		for(j = i+1; j < size; j++)
+		{
+			if(array[j] < array[min])
+			{
+				int min = j;
+			}
+		}
+		if(min != j)
 	  {
-	    
-            if(array[j] < array[min])
-	      {
-		
-               int min = j;
-            }
-        }
-        if(min != j)
-	  {
-	    
-            // swapp
-            int temp = array[j];
-            array[j] = array[j+1];
-            array[j+1] = temp;
-        }
+		  /*swap*/
+		  int temp = array[j];
+		  array[j] = array[j+1];
+		  array[j+1] = temp;
+	  }
     }
 }
