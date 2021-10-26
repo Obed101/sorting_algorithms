2d4548edc7ee04698b5556ddc9ea6641192eade9
Obed101 <obedamoako92@gmail.com>
2021-10-26 13:57:25 -0700

    on it

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,30 +5,7 @@ void sortedInsert(listint_t **head_ref, listint_t *newNode)
 
 	listint_t *current;
 	/*if list is empty*/
-	if (*head_ref == NULL)
-		*head_ref = newNode;
 
-	if ((*head_ref)->n >= newNode->n)
-	{
-		newNode->next = *head_ref;
-		newNode->next->prev = newNode;
-		*head_ref = newNode;
-	}
-	else
-	{
-		current = *head_ref;
-
-		while (current->next && current->next->n < newNode->n)
-			current = current->next;
-		print_list(*head_ref);
-		newNode->next = current->next;
-
-		if (current->next != NULL)
-			newNode->next->prev = newNode;
-		print_list(*head_ref);
-		current->next = newNode;
-		newNode->prev = current;
-	}
     if (*head_ref == NULL)
         *head_ref = newNode;
     else if ((*head_ref)->n >= newNode->n)
