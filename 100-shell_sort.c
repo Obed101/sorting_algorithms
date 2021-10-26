66db08cc9400931c57de533355c1e275d31b9563
Obed101 <obedamoako92@gmail.com>
2021-10-26 12:18:44 -0700

    betty, compilation

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,27 +1,27 @@
 #include "sort.h"
 /**
-* shell_sort - a function that uses the shell sort algo to sort
-* @array: the array of items to sort
-* @size: sizeof the array
-*/
+ * shell_sort - a function that uses the shell sort algo to sort
+ * @array: the array of items to sort
+ * @size: sizeof the array
+ */
 void shell_sort(int *array, size_t size)
 {
 	if (size < 2)
 	{
- 	   return;
+		return;
  	}
 	int i, j, k, inc;
 	inc = 1;
 
 	while (inc <= (size - 1) / 9)
-	      inc = 3 * inc + 1;
+		inc = 3 * inc + 1;
 	while (inc >= 1)
 	{
 		for (i = inc; i < size; i++)
 		{
 			k = array[i];
 			for (j = i - inc; j >= 0 && k < array[j]; j = j - inc)
-			array[j + inc] = k;
+				array[j + inc] = k;
 			print_array(array, size);
 		}
 		inc /= 3;
