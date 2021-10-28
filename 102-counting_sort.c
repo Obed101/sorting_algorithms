511574ab398b47d157fd09a97fedf216cde46b91
Obed101 <obedamoako92@gmail.com>
2021-10-27 23:42:02 -0700

    fixed compilation problems with sort.h

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,27 +2,23 @@
 #define RANGE 255
 #include <string.h>
 
-void counting_sort(int *array, size_t size);
-
 void counting_sort(int *array, size_t size)
 {
 	int out[size], i, count[RANGE + 1];
-	size = sizeof(array) / sizeof(array[0]);
-	
+
 	memset(count, 0, sizeof(count));
 	for (i = 0; array[i]; ++i)
-	++count[array[i]];
-	
+		++count[array[i]];
+
 	for (i = 1; i <= RANGE; ++i)
-	count[i] += count[i - 1];
-	
+		count[i] += count[i - 1];
+
 	for (i = 0; array[i], ++i;)
 	{
 		out[count[array[i]] - 1] = array[i];
 		--count[array[i]];
 	}
-	
 	for (i = 0; array[i]; ++i)
-	array[i] = out[i];
+		array[i] = out[i];
 	print_array(array, size);
 }
--- sort.h
+++ sort.h
@@ -1,12 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
-
 void swap(int *x, int *y);
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -21,6 +18,45 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
+
+typedef enum kind_e
+{
+    SPADE = 0,
+    HEART,
+    CLUB,
+    DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+    const char *value;
+    const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+    const card_t *card;
+    struct deck_node_s *prev;
+    struct deck_node_s *next;
+} deck_node_t;
+
+
+
 void sortedInsert(listint_t **head_ref, listint_t *newNode);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -28,6 +64,15 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
 
 #endif
