fe90975cf31ef2f5f8205f6d1381db64eec70a49
Obed101 <obedamoako92@gmail.com>
2021-10-30 08:58:59 -0700

    implemented the counting sort algorithm

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -12,21 +12,76 @@
 
 void counting_sort(int *array, size_t size)
 {
-	int out[size], i, count[RANGE + 1];
+	int *counter, *node, i, j, k;
 
-	memset(count, 0, sizeof(count));
-	for (i = 0; array[i]; ++i)
-		++count[array[i]];
+	if (array == NULL || size < 2)
+		return;
 
-	for (i = 1; i <= RANGE; ++i)
-		count[i] += count[i - 1];
+	node = malloc(sizeof(int) * ((int)size + 1));
+	if (node == NULL)
+		return;
+	k = r_largest(array, size);
+	counter = malloc(sizeof(int) * (k + 1));
+	for (i = 0; i < k + 1; i++)
+		counter[i] = 0;
 
-	for (i = 0; array[i], ++i;)
+	if (!counter)
 	{
-		out[count[array[i]] - 1] = array[i];
-		--count[array[i]];
+		free(node);
+		return;
 	}
-	for (i = 0; array[i]; ++i)
-		array[i] = out[i];
-	print_array(array, size);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		j = array[i];
+		counter[j] += 1;
+	}
+	for (i = 1; i <= k + 1; i++)
+		counter[i] += counter[i - 1];
+	print_array(counter, k + 1);
+	for (i = (int)size - 1; i >= 0; i--)
+	{
+		j = array[i];
+		counter[j] -= 1;
+		node[counter[j]] = array[i];
+	}
+	free(counter);
+	copy_elements(array, node, size);
+}
+
+
+/**
+ * r_largest - retrieves the largest element
+ * @array: the array
+ * @size: size of the array
+ * Return: the largest number (int)
+ */
+
+int r_largest(int *array, size_t size)
+{
+	int i, b = 0;
+
+	for (i = 0; i < (int)size; i++)
+		b = b < array[i] ? array[i] : b;
+
+	return (b);
+}
+
+
+/**
+ * copy_elements - fills array a with values in array b
+ * @arra: array a
+ * @arrb: array b
+ * @size: size both arrays should be same size
+ */
+
+int copy_elements(int *arr1, int *arr2, size_t size)
+{
+	int i;
+
+	for (i = 0; i < (int)size; i++)
+		arr1[i] = arr2[i];
+
+	free(arr2);
+	return (0);
 }
